7f2f5643bc183853499e15cdb5a55a70f77855c0
prospectprospekt <prospectprospekt8901@gmail.com>
2024-09-19 15:44:00 -0400

    Update fork.py

--- fork.py
+++ fork.py
@@ -23,7 +23,7 @@ while 1 == 1:
     pagetypes = []
     one = print("enter id: ")
     two = print("enter number of pages ")
-    get_page_types(full_text_id, page_num)
+    get_page_types(one, two)
     print(pagetypes)
     three = print("end the machine?"[yn])
     if three == "y":
