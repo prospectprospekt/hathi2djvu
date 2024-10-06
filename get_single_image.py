d5bd5f8f28c5c5e5a7f3d5457449ce49eca00e61
prospectprospekt <prospectprospekt8901@gmail.com>
2024-10-06 00:06:39 -0400

    Update get_single_image.py

--- get_single_image.py
+++ get_single_image.py
@@ -11,10 +11,11 @@ def get_single_hathitrust_image(full_text_id, page_num, orientation):
       print("Getting page (upward orientation)")
       response = requests.get(url_upright)
       if response.status_code == 200:
-      image_path = f"{page_num}_upright.png"
-      with open(image_path, 'wb') as file:
-        file.write(response.content)
+        image_path = f"{page_num}_upright.png"
+        with open(image_path, 'wb') as file:
+          file.write(response.content)
         print(f"Upright image for #{page_num} saved successfully!")
+        break
       print("Download failed. Trying again.")
   elif orientation == "upside down":
     url_upside_down = f"https://babel.hathitrust.org/cgi/imgsrv/image?id={full_text_id};seq={page_num};size=full;format=image/png;rotation=180"
@@ -26,5 +27,6 @@ def get_single_hathitrust_image(full_text_id, page_num, orientation):
         with open(image_path, 'wb') as file:
           file.write(response.content)
         print(f"Upright image for #{page_num} saved successfully!")
+        break
       print("Download failed. Trying again.")
 get_single_hathitrust_image("osu.32435055416200", "1", "upward")
